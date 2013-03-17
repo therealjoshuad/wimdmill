c206dcc9c343e5bccce4a92f46c75518cf0ec37a
therealjoshuad <joshua.delaughter@gmail.com>
2013-03-17 16:26:19 -0500

    Create remoteimage.bat

---
+++ remoteimage.bat
@@ -0,0 +1,6 @@
+diskpart /s diskcreate.txt
+set servername=SERVERNAME
+net use Q: "\\%servername%\System Images" /user:USERNAME PASSWORD
+imagex /apply Q:\install.wim 1 W:\
+w:\windows\system32\bcdboot W:\windows /l en-us
+exit
